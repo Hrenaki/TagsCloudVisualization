b193c297b0e8c0c0f71d61c7dec32faa84b1d97b
Hrenaki <shikhovwtr@mail.ru>
2022-12-05 13:44:23 +0300

    Add project files.

---
+++ TagsCloudVisualization.Tests/CircularCloudLayouterTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization.Tests
+{
+   public class CircularCloudLayouterTests
+   {
+      [Test]
+      public void PutNextRectangle_Should_ReturnZeroSizeRectangleInZeroPoint_WhenSizeIsZeroAndCenterIsZero()
+      {
+         var center = Point.Empty;
+         var size = Size.Empty;
+         var layouter = CircularCloudLayouterFactory.Get(center);
+         var expectedRectangle = new Rectangle(center, size);
+
+         var actualRectangle = layouter.PutNextRectangle(size);
+
+         actualRectangle.Should().Be(expectedRectangle);
+      }
+
+      [Test]
+      public void PutNextRectanlge_Should_ReturnNonZeroRectangleInZeroPoint_WhenSizeIsNotZeroAndCenterIsZero()
+      {
+         var center = Point.Empty;
+         var size = new Size(100, 100);
+         var layouter = CircularCloudLayouterFactory.Get(center);
+         var expectedRectangle = new Rectangle(center, size);
+
+         var actualRectangle = layouter.PutNextRectangle(size);
+
+         actualRectangle.Should().Be(expectedRectangle);
+      }
+   }
+
+   internal static class CircularCloudLayouterFactory
+   {
+      public static CircularCloudLayouter Get(Point center)
+      {
+         return new CircularCloudLayouter(center);
+      }
+   }
+}
